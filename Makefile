b64952ae3100e7f289cb73807d8ba9efad541c05
Yusuke Hata <42143893+octu0@users.noreply.github.com>
2026-02-21 12:37:27 +0900

    add env{WASM_BUILD}

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 .PHONY: wasm
 wasm:
-	swift package --swift-sdk swift-6.2.3-RELEASE_wasm -c release plugin --allow-writing-to-package-directory js --use-cdn --product wasm
+	WASM_BUILD=1 swift package --swift-sdk swift-6.2.3-RELEASE_wasm -c release plugin --allow-writing-to-package-directory js --use-cdn --product wasm
 	wasm-opt -O4 --fast-math --converge .build/plugins/PackageToJS/outputs/Package/wasm.wasm -o .build/plugins/PackageToJS/outputs/Package/wasm.wasm
 	
--- Package.swift
+++ Package.swift
@@ -1,31 +1,39 @@
 // swift-tools-version: 6.0
 import PackageDescription
+import Foundation
 
-let package = Package(
-    name: "veif",
-    platforms: [
-        .macOS(.v14)
-    ],
-    products: [
-        .library(name: "veif", targets: ["veif"]),
-        .executable(name: "wasm", targets: ["wasm"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/swiftwasm/JavaScriptKit", from: "0.45.0")
-    ],
-    targets: [
-        .target(
-            name: "veif",
-            dependencies: [],
-            swiftSettings: [
-                .unsafeFlags(["-Ounchecked", "-wmo", "-Xcc", "-msimd128"], .when(platforms: [.wasi]))
-            ]
-        ),
-        .executableTarget(
-            name: "example",
-            dependencies: ["veif"],
-            //swiftSettings:[.unsafeFlags(["-whole-module-optimization"])]
-        ),
+let isWasmBuild = ProcessInfo.processInfo.environment["WASM_BUILD"] == "1"
+
+var packageProducts: [Product] = [
+    .library(name: "veif", targets: ["veif"]),
+]
+
+var packageDeps: [Package.Dependency] = []
+
+var packageTargets: [Target] = [
+    .target(
+        name: "veif",
+        dependencies: [],
+        swiftSettings: [
+            .unsafeFlags(["-Ounchecked", "-wmo", "-Xcc", "-msimd128"], .when(platforms: [.wasi]))
+        ]
+    ),
+    .executableTarget(
+        name: "example",
+        dependencies: ["veif"],
+        //swiftSettings:[.unsafeFlags(["-whole-module-optimization"])]
+    ),
+    .testTarget(
+        name: "veifTests",
+        dependencies: ["veif"]
+    ),
+]
+
+if isWasmBuild {
+    packageDeps.append(.package(url: "https://github.com/swiftwasm/JavaScriptKit", from: "0.45.0"))
+    packageProducts.append(.executable(name: "wasm", targets: ["wasm"]))
+    
+    packageTargets.append(
         .executableTarget(
             name: "wasm",
             dependencies: [
@@ -40,11 +48,17 @@ let package = Package(
             plugins: [
                 .plugin(name: "BridgeJS", package: "JavaScriptKit")
             ]
-        ),
-        .testTarget(
-            name: "veifTests",
-            dependencies: ["veif"]
-        ),
+        )
+    )
+}
+
+let package = Package(
+    name: "veif",
+    platforms: [
+        .macOS(.v15)
     ],
-    swiftLanguageModes: [.v6],
+    products: packageProducts,
+    dependencies: packageDeps,
+    targets: packageTargets,
+    swiftLanguageModes: [.v6]
 )
